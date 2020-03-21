1350e11fb00070dbf07e2f1723fb4133e25fb8b8
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-03-21 19:53:51 +0530

    Calculate Daily Employee Wage

--- empWageCompution.sh
+++ empWageCompution.sh
@@ -8,3 +8,16 @@ echo "Employee is present "
 else
 echo "Employee is absent"
 fi
+
+
+PER_HOUR=20
+
+dailyEmployeeWage()
+{
+day_hour=8
+daily_wage=$(( $PER_HOUR * $day_hour |bc -l ))
+echo "Hours of Full day Employee is : $day_hour"
+echo "Dialy Wage of the Employee  is: $daily_wage"
+}
+
+dailyEmployeeWage
